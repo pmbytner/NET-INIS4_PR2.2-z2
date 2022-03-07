0be7922f3094093b9f602df982c0f60024131b63
PMBytner <pawel.m.bytner@gmail.com>
2022-03-07 08:48:57 +0100

    Dodaj pliki projektów.

---
+++ Dane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_INIS4_PR2._2_z2
+{
+    public class Dane
+    {
+        public string Imię { get; set; } = "Przemek";
+        
+    }
+}

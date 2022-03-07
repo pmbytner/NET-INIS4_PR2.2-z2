13597b4cb7d390861c3a9c1e44a7daf621d3a5a6
PMBytner <pawel.m.bytner@gmail.com>
2022-03-07 09:06:04 +0100

    implementacja interfejsu INotifyPropertyChanged i wiązanie do jawnie implementowanych właściwości w celu np formatowania komunikatu

--- Dane.cs
+++ Dane.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NET_INIS4_PR2._2_z2
 {
-    public class Dane
+    public class Dane : INotifyPropertyChanged
     {
-        public string Imię { get; set; } = "Przemek";
-        
+        string imię = "Przemek";
+        public string Imię
+        {
+            get { return imię; }
+            set
+            {
+                imię = value;
+                OnPropertyChanged();
+                OnPropertyChanged("Format");
+            }
+        }
+        public string Format {
+            get {
+                return $"Podane imię to {Imię}.";
+            } 
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
